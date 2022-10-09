3b7f9c982a88846785289ade92b73a0511d7d42d
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2022-10-09 14:55:11 +0500

    Initialization and Short Hand

--- Add Code Here/C++/Search.cpp
+++ Add Code Here/C++/Search.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main(){
     int a[]={1,2};
-    int n;
+    int n=NULL;
     cin>>n;
 
     for (int i=0;i<sizeof(a);i++){
-        if(n==a[i])
-        cout<<"Found";
+        if(n==a[i])?cout<<"Found":cout<<"Not Found";
     }
 
     return 0;
